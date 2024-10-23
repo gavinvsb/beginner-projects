9394d2420223da3e682a58070e36e53e30ab870f
gavinvsb <82419723+gavinvsb@users.noreply.github.com>
2024-10-23 10:59:00 -0700

    Update and rename prime-numbers-list to prime_numbers_list.py

--- prime_numbers_list.py
+++ prime_numbers_list.py
@@ -1,6 +1,8 @@
-# Given k numbers which are less than n, return the set of prime number among them
-# Note: The task is to write a program to print all Prime numbers in an Interval.
-# Definition: A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself. 
+"""
+Given k numbers which are less than n, return the set of prime number among them
+Note: The task is to write a program to print all Prime numbers in an Interval.
+Definition: A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself. 
+"""
 
 n = 35
 def solution(n):
