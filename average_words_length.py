1159e4afb51949dda251c12264a2595799a3910b
gavinvsb <82419723+gavinvsb@users.noreply.github.com>
2024-10-23 10:43:13 -0700

    Update and rename average-words-length to average_words_length.py

--- average_words_length.py
+++ average_words_length.py
@@ -1,7 +1,8 @@
-# For a given sentence, return the average word length. 
-# Note: Remember to remove punctuation first.
+"""For a given sentence, return the average word length. 
+* Note: Remember to remove punctuation first.
+"""
 
-sentence1 = "Hi all, my name is Tom...I am originally from Australia."
+sentence1 = "Hi all, my name is Gavin...I am originally from the United States."
 sentence2 = "I need to work very hard to learn more about algorithms in Python!"
 
 def solution(sentence):
