9c50aefda48f80fcfec237a62b81b37e305a8edc
gavinvsb <82419723+gavinvsb@users.noreply.github.com>
2024-10-23 10:59:50 -0700

    Update and rename reverse-integer to reverse_integer.py

--- reverse_integer.py
+++ reverse_integer.py
@@ -1,5 +1,7 @@
-# Given an integer, return the integer with reversed digits.
-# Note: The integer could be either positive or negative.
+"""
+Given an integer, return the integer with reversed digits.
+Note: The integer could be either positive or negative.
+"""
 
 def solution(x):
     string = str(x)
